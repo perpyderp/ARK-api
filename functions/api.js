98412449274946bd8835abf651646c2adec42cb3
perpyderp <g4m3rall3n@gmail.com>
2023-05-05 23:45:44 -0400

    changed routes

--- functions/api.js
+++ functions/api.js
@@ -2,31 +2,26 @@
 const express = require('express');
 const router = express.Router();
 const serverless = require('serverless-http');
-
 const creatureRoutes = require('../src/routes/creatureRoutes');
-// const db = require('../src/db');
+const db = require('../src/db');
 
 require('dotenv');
 
 const app = express()
 const port = process.env.SERVER_PORT;
-app.use('/.netlify/functions/api', creatureRoutes);
+app.use('/api', creatureRoutes);
 app.use(express.json());
 
 router.get('/', (req, res) => {
   res.send('Welcome to the ARK API');
 });
 
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-// db.once('open', () => {
-//   console.log('MongoDB connection established');
-//   // Start server once connected to database
-//   app.listen(port, () => {
-//     console.log(`ARK API listening on port ${port}`);
-//   })
-// });
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('MongoDB connection established');
+});
 
-app.use('/.netlify/functions/api', router);
+app.use('/', router);
 
 // Export the Express app as a serverless function
 module.exports.handler = serverless(app);
