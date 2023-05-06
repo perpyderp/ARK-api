1a48b7c706bca05a390401d152d1a90d6f728f8d
perpyderp <g4m3rall3n@gmail.com>
2023-05-06 00:39:27 -0400

    changed database env path

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: './.env' });
+dotenv.config({ path: './env' });
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
