31aa18651d9191dd2e398215912fbdc125b60d59
perpyderp <g4m3rall3n@gmail.com>
2023-05-05 23:32:52 -0400

    edited api

--- functions/api.js
+++ functions/api.js
@@ -1,7 +1,10 @@
+'use strict';
 const express = require('express');
+const router = express.Router();
+const serverless = require('serverless-http');
 
 const creatureRoutes = require('../src/routes/creatureRoutes');
-const db = require('../src/db');
+// const db = require('../src/db');
 
 require('dotenv');
 
@@ -10,21 +13,20 @@ const port = process.env.SERVER_PORT;
 app.use('/api', creatureRoutes);
 app.use(express.json());
 
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
   res.send('Welcome to the ARK API');
 });
 
-console.log("Connecting to database...");
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('MongoDB connection established');
-  // Start server once connected to database
-  app.listen(port, () => {
-    console.log(`ARK API listening on port ${port}`);
-  })
-});
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.once('open', () => {
+//   console.log('MongoDB connection established');
+//   // Start server once connected to database
+//   app.listen(port, () => {
+//     console.log(`ARK API listening on port ${port}`);
+//   })
+// });
+
+app.use('/.netlify/functions/api', router);
 
 // Export the Express app as a serverless function
-module.exports.handler = async (event, context) => {
-    return await app(event, context);
-};
+module.exports.handler = serverless(app);
